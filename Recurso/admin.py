5048b6345ddca6c19ac9231fe10fff03f56089ae
Cabos <dario.caboz@hotmail.com>
2021-04-29 11:08:34 +0100

    version 0.1

--- Recurso/admin.py
+++ Recurso/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from Recurso.models import *
+
+admin.site.register(Sala)
+admin.site.register(Campus)
+admin.site.register(Edificio)
+admin.site.register(Equipamento)
+admin.site.register(Recurso)
+admin.site.register(Serviço)
+admin.site.register(TipoDeEquipamento)
+admin.site.register(TipoDeRecurso)
+admin.site.register(TipoDeServiço)
